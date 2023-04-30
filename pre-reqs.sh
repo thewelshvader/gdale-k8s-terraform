3275777fc9d4d8d88ed78eb672b84af08f2b1fbb
Kevin Delaney <thewelshvader@hotmail.com>
2023-04-30 19:06:54 +0100

    started dev branch with prereqs

---
+++ pre-reqs.sh
@@ -0,0 +1,6 @@
+wget -q --show-progress --https-only --timestamping \
+  https://storage.googleapis.com/kubernetes-the-hard-way/cfssl/1.4.1/linux/cfssl \
+  https://storage.googleapis.com/kubernetes-the-hard-way/cfssl/1.4.1/linux/cfssljson
+
+chmod +x cfssl cfssljson
+mv cfssl cfssljson /usr/local/bin/
