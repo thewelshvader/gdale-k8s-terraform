3f79391dd7bb7ef8d7697c494940a30d13749d89
Kevin Delaney <Kevin.Delaney@blueyonder.com>
2023-05-02 16:26:39 +0100

    adding vnet config

--- tf/main.tf
+++ tf/main.tf
@@ -13,8 +13,17 @@ resource "azurerm_resource_group" "k8s-native" {
   name     = "rsg-${var.suffix}"
   location = var.location
 }
-resource "azurerm_network_security_group" "example" {
+resource "azurerm_network_security_group" "k8s-native" {
   name                = "nsg-${var.suffix}"
   location            = azurerm_resource_group.k8s-native.location
   resource_group_name = azurerm_resource_group.k8s-native.name
 }
+resource "azurerm_virtual_network" "k8s-native" {
+  name                = "vnet-${var.suffix}"
+  location            = azurerm_resource_group.k8s-native.location
+  resource_group_name = azurerm_resource_group.k8s-native.name
+  address_space       = ["10.0.0.0/16"]
+  subnet {
+    name           = "snet-${var.suffix}"
+    address_prefix = "10.0.1.0/24"
+  }
