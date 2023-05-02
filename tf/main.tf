a6a4f8768e3a1434b2dd91e2e3c549bde75d2503
Kevin Delaney <thewelshvader@hotmail.com>
2023-05-02 12:24:59 +0100

    planning - nsg

--- tf/main.tf
+++ tf/main.tf
@@ -13,3 +13,8 @@ resource "azurerm_resource_group" "k8s-native" {
   name     = "rsg-${var.suffix}"
   location = var.location
 }
+resource "azurerm_network_security_group" "example" {
+  name                = "nsg-${var.suffix}"
+  location            = azurerm_resource_group.k8s-native.location
+  resource_group_name = azurerm_resource_group.k8s-native.name
+}
