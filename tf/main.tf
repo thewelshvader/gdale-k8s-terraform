23e7464106a8cd70775126c09518bd262278ab15
Kevin Delaney <Kevin.Delaney@blueyonder.com>
2023-05-02 17:23:28 +0100

    moving subnet to new task

--- tf/main.tf
+++ tf/main.tf
@@ -23,17 +23,17 @@ resource "azurerm_virtual_network" "k8s-native" {
   location            = azurerm_resource_group.k8s-native.location
   resource_group_name = azurerm_resource_group.k8s-native.name
   address_space       = ["10.0.0.0/16"]
-  subnet {
-    name           = "snet-${var.suffix}"
-    address_prefix = "10.0.1.0/24"
-    security_group = azurerm_network_security_group.k8s-native.name
-  }
+}
+resource "azurerm_subnet" "k8s-native" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.k8s-native.name
+  virtual_network_name = azurerm_virtual_network.k8s-native.name
+  address_prefixes     = ["10.0.2.0/24"]
 }
 resource "random_id" "vm_name_unique" {
   count = "${var.vm_count}"
   byte_length = 4
 }
-
 resource "azurerm_network_interface" "k8s-native" {
   count               = var.vm_count
   name                = element(random_id.vm_name_unique.*.hex, count.index)
@@ -42,7 +42,7 @@ resource "azurerm_network_interface" "k8s-native" {
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_virtual_network.k8s-native.subnet["id"]
+    subnet_id                     = azurerm_subnet.k8s-native.id
     private_ip_address_allocation = "Dynamic"
   }
 }
